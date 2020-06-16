34ca7af9074dcde658b464ff51eb27446f5316c8
MuhammadTalib <talibwaseem135@gmail.com>
2020-06-17 01:09:31 +0500

    Updates

---
+++ precache-manifest.3a8cef300390b9fcad8703fb783f0f3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa08ab9e59116af2f3749eeecd5cf88",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "a7d2016bc32cba22e9b9",
+    "url": "/my-portfolio/static/css/main.708ef112.chunk.css"
+  },
+  {
+    "revision": "a52423379186f6721467",
+    "url": "/my-portfolio/static/js/2.ac0ca5ac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-portfolio/static/js/2.ac0ca5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d2016bc32cba22e9b9",
+    "url": "/my-portfolio/static/js/main.2b97cc72.chunk.js"
+  },
+  {
+    "revision": "feb14810df33d645b05c",
+    "url": "/my-portfolio/static/js/runtime-main.9efc66b9.js"
+  },
+  {
+    "revision": "c2bbe936c008ed63c46f417aa46588e3",
+    "url": "/my-portfolio/static/media/mypic.c2bbe936.jpg"
+  }
+]);
