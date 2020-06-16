8a88196c1779f39b82f26c04465462f2e13ebdf1
MuhammadTalib <talibwaseem135@gmail.com>
2020-06-17 01:00:12 +0500

    first release

--- src/Components/projects.jsx
+++ src/Components/projects.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { useCallback } from 'react';
-
 class Projects extends Component {
     state = {  }
     render() { 
@@ -10,67 +8,61 @@ class Projects extends Component {
             <div className="project-headers">React Projects:</div>
             <ul className="projects-main-div">
                 <li><a href={"https://github.com/MuhammadTalib/Chatbot"}>Chatbot</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Softboard"}>Softboard</a></li>
                 <li><a href={"https://github.com/MuhammadTalib/my-portfolio"}>Portfolio</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Smart-Compiler"}>Smart Compiler</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Softboard"}>Softboard</a></li>
                 <li><a href={"https://github.com/MuhammadTalib/react-pen-draw"}>Pen Drawing</a></li>
                 <li><a href={"https://github.com/MuhammadTalib/Vidly"}>Vidly</a></li>
                 <li><a href={"https://github.com/MuhammadTalib/custom-linear-progressbar"}>Custom Linear Progressbar</a></li>
                 <li><a href={"https://github.com/MuhammadTalib/react-quiz-app"}>Quiz App</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/my-portfolio"}>Portfolio</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/react-pen-draw"}>Pen Drawing</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Vidly"}>Vidly</a></li>
             </ul>
 
             <div className="project-headers">Nodejs Projects:</div>
-            <div className="projects-main-div">
-                <div><a href={"https://github.com/MuhammadTalib/chat-app-node"}>Chat App (Socket.io)</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/task-manager"}>Task Manager</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/my-portfolio"}>Portfolio</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/react-pen-draw"}>Pen Drawing</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Vidly"}>Vidly</a></div>
-            </div>
+            <ul className="projects-main-div">
+                <li><a href={"https://github.com/MuhammadTalib/chat-app-node"}>Chat App (Socket.io)</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/task-manager"}>Task Manager</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/NotesApp"}>Notes App</a></li>
+            </ul>
 
             <div className="project-headers">Computer Graphics Projects(Javascript):</div>
-            <div className="projects-main-div">
-                <div><a href={"https://github.com/MuhammadTalib/Shapes"}>Shapes Drawing</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Snake-Game"}>Snake Game</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Polygonia"}>Polygon Drawing</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Image-Tweening"}>Image Tweening</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Object-Identifier-in-Image"}>Object Identifier</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Wall-Clock-Scene"}>Wall Clock Scene (Three.js)</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/sierpinski-gasket"}>Sierpinski Gasket (Three.js)</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Object-Identifier-in-Image"}>Object Identifier</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Object-Identifier-in-Image"}>Object Identifier</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Object-Identifier-in-Image"}>Object Identifier</a></div>
-            </div>
+            <ul className="projects-main-div">
+                <li><a href={"https://github.com/MuhammadTalib/Shapes"}>Shapes Drawing</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Snake-Game"}>Snake Game</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Polygonia"}>Polygon Drawing</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Image-Tweening"}>Image Tweening</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Object-Identifier-in-Image"}>Object Identifier</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Wall-Clock-Scene"}>Wall Clock Scene (Three.js)</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/sierpinski-gasket"}>Sierpinski Gasket (Three.js)</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Shape-Tweening"}>Shape Tweening</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Parametric-Curve"}>Parametric Curves</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Polar-Curves"}>Polar Curves</a></li>
+
+            </ul>
 
 
             <div className="project-headers">Android (Kotlin) Projects:</div>
-            <div className="projects-main-div">
+            <ul className="projects-main-div">
                 <a href={"https://github.com/MuhammadTalib/Chat-Club"}>Chat Club</a>
-            </div>
+            </ul>
 
             <div className="project-headers">Data Structures Projects (C++):</div>
-            <div className="projects-main-div">
-                <div><a href={"https://github.com/MuhammadTalib/Krushkal-Algorithm"}>Krushkal Algorithm</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Prims-Algorithm"}>Prims Algorithm</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Infix-to-Postfix"}>Infix To Postfix</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Hashing-Data-Structure"}>Hashing</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Queue"}>Queue</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Quick-Sort"}>Quick Sort</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Queue"}>Queue</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Queue"}>Queue</a></div>
-                <div><a href={"https://github.com/MuhammadTalib/Queue"}>Queue</a></div>
-            </div>
+            <ul className="projects-main-div">
+                <li><a href={"https://github.com/MuhammadTalib/Krushkal-Algorithm"}>Krushkal Algorithm</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Prims-Algorithm"}>Prims Algorithm</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Infix-to-Postfix"}>Infix To Postfix</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Hashing-Data-Structure"}>Hashing</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Queue"}>Queue</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Quick-Sort"}>Quick Sort</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Stack"}>Stack</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Heap-DataStructure"}>Heap Data Structure</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/AVL-Tree"}>AVL Tree</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Binary-Search-Tree"}>Binary Search Tree</a></li>
+                <li><a href={"https://github.com/MuhammadTalib/Circular-Linked-List"}>Circular Linked List</a></li>
+
+            </ul>
             
         </div>
 
-
-
-        {/* <div className="projects-card" onClick={()=> window.open("https://github.com/MuhammadTalib/Chat-Club", "_blank")}>
-            <div className="project-name">ChatClub</div>
-            <div className="project-disc">Chat Club is Android application it's features are similar to facebook. It is based on Kotlin and made on Android Studio.</div>
-        </div> */}
     </div> );
     }
 }
