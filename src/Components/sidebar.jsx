d018fde867a16313011c8fed55aa9a3306d66a75
Muhammad Talib Waseem <36582251+MuhammadTalib@users.noreply.github.com>
2022-01-26 00:54:44 +0500

    Update sidebar.jsx

--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
-import mypic from "../Images/mypic.jpg";
+import mypic from "../Images/mypic.jpg";  
 
 class SideBar extends Component {
   render() {
