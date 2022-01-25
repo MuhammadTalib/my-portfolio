1be433ee48af9bc5271a2968b06106dcae594904
Muhammad Talib Waseem <36582251+MuhammadTalib@users.noreply.github.com>
2022-01-25 23:50:24 +0500

    url

--- src/Components/projects.jsx
+++ src/Components/projects.jsx
@@ -1,71 +1,198 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Projects extends Component {
-    state = {  }
-    render() { 
-        return ( <div className="about-main">
+  state = {};
+  render() {
+    return (
+      <div className="about-main">
         <div className="page-header">Projects</div>
         <div className="main-project-wrapper">
-            <div className="project-headers">React Projects:</div>
-            <ul className="projects-main-div">
-                <li><a href={"https://github.com/MuhammadTalib/Chatbot"}>Chatbot</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/my-portfolio"}>Portfolio</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Smart-Compiler"}>Smart Compiler</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Softboard"}>Softboard</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/react-pen-draw"}>Pen Drawing</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Vidly"}>Vidly</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/custom-linear-progressbar"}>Custom Linear Progressbar</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/react-quiz-app"}>Quiz App</a></li>
-            </ul>
+          <div className="project-headers">React Projects:</div>
+          <ul className="projects-main-div">
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Chatbot"}>Chatbot</a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/my-portfolio"}>
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a href={"https://muhammadtalib.github.io/Smart-Compiler/"}>
+                Smart Compiler
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Softboard"}>
+                Softboard
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/react-pen-draw"}>
+                Pen Drawing
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Vidly"}>Vidly</a>
+            </li>
+            <li>
+              <a
+                href={
+                  "https://github.com/MuhammadTalib/custom-linear-progressbar"
+                }
+              >
+                Custom Linear Progressbar
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/react-quiz-app"}>
+                Quiz App
+              </a>
+            </li>
+          </ul>
 
-            <div className="project-headers">Nodejs Projects:</div>
-            <ul className="projects-main-div">
-                <li><a href={"https://github.com/MuhammadTalib/chat-app-node"}>Chat App (Socket.io)</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/task-manager"}>Task Manager</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/NotesApp"}>Notes App</a></li>
-            </ul>
+          <div className="project-headers">Nodejs Projects:</div>
+          <ul className="projects-main-div">
+            <li>
+              <a href={"https://github.com/MuhammadTalib/chat-app-node"}>
+                Chat App (Socket.io)
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/task-manager"}>
+                Task Manager
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/NotesApp"}>
+                Notes App
+              </a>
+            </li>
+          </ul>
 
-            <div className="project-headers">Computer Graphics Projects(Javascript):</div>
-            <ul className="projects-main-div">
-                <li><a href={"https://github.com/MuhammadTalib/Shapes"}>Shapes Drawing</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Snake-Game"}>Snake Game</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Polygonia"}>Polygon Drawing</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Image-Tweening"}>Image Tweening</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Object-Identifier-in-Image"}>Object Identifier</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Wall-Clock-Scene"}>Wall Clock Scene (Three.js)</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/sierpinski-gasket"}>Sierpinski Gasket (Three.js)</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Shape-Tweening"}>Shape Tweening</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Parametric-Curve"}>Parametric Curves</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Polar-Curves"}>Polar Curves</a></li>
+          <div className="project-headers">
+            Computer Graphics Projects(Javascript):
+          </div>
+          <ul className="projects-main-div">
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Shapes"}>
+                Shapes Drawing
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Snake-Game"}>
+                Snake Game
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Polygonia"}>
+                Polygon Drawing
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Image-Tweening"}>
+                Image Tweening
+              </a>
+            </li>
+            <li>
+              <a
+                href={
+                  "https://github.com/MuhammadTalib/Object-Identifier-in-Image"
+                }
+              >
+                Object Identifier
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Wall-Clock-Scene"}>
+                Wall Clock Scene (Three.js)
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/sierpinski-gasket"}>
+                Sierpinski Gasket (Three.js)
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Shape-Tweening"}>
+                Shape Tweening
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Parametric-Curve"}>
+                Parametric Curves
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Polar-Curves"}>
+                Polar Curves
+              </a>
+            </li>
+          </ul>
 
-            </ul>
+          <div className="project-headers">Android (Kotlin) Projects:</div>
+          <ul className="projects-main-div">
+            <a href={"https://github.com/MuhammadTalib/Chat-Club"}>Chat Club</a>
+          </ul>
 
-
-            <div className="project-headers">Android (Kotlin) Projects:</div>
-            <ul className="projects-main-div">
-                <a href={"https://github.com/MuhammadTalib/Chat-Club"}>Chat Club</a>
-            </ul>
-
-            <div className="project-headers">Data Structures Projects (C++):</div>
-            <ul className="projects-main-div">
-                <li><a href={"https://github.com/MuhammadTalib/Krushkal-Algorithm"}>Krushkal Algorithm</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Prims-Algorithm"}>Prims Algorithm</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Infix-to-Postfix"}>Infix To Postfix</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Hashing-Data-Structure"}>Hashing</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Queue"}>Queue</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Quick-Sort"}>Quick Sort</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Stack"}>Stack</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Heap-DataStructure"}>Heap Data Structure</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/AVL-Tree"}>AVL Tree</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Binary-Search-Tree"}>Binary Search Tree</a></li>
-                <li><a href={"https://github.com/MuhammadTalib/Circular-Linked-List"}>Circular Linked List</a></li>
-
-            </ul>
-            
+          <div className="project-headers">Data Structures Projects (C++):</div>
+          <ul className="projects-main-div">
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Krushkal-Algorithm"}>
+                Krushkal Algorithm
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Prims-Algorithm"}>
+                Prims Algorithm
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Infix-to-Postfix"}>
+                Infix To Postfix
+              </a>
+            </li>
+            <li>
+              <a
+                href={"https://github.com/MuhammadTalib/Hashing-Data-Structure"}
+              >
+                Hashing
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Queue"}>Queue</a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Quick-Sort"}>
+                Quick Sort
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Stack"}>Stack</a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Heap-DataStructure"}>
+                Heap Data Structure
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/AVL-Tree"}>AVL Tree</a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Binary-Search-Tree"}>
+                Binary Search Tree
+              </a>
+            </li>
+            <li>
+              <a href={"https://github.com/MuhammadTalib/Circular-Linked-List"}>
+                Circular Linked List
+              </a>
+            </li>
+          </ul>
         </div>
-
-    </div> );
-    }
+      </div>
+    );
+  }
 }
- 
+
 export default Projects;
